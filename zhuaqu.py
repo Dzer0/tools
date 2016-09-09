0a9afdd76563ed845ba95afd1f46b5ea222ea217
Dzer0 <23442435@qq.com>
2016-09-09 23:39:14 +0800

    1、添加了新的获取宿主机的方法 2、添加获取单个容器内的信息 3、http://API:port/api/v1.0/machine 可以获取地址

--- zhuaqu.py
+++ zhuaqu.py
@@ -6,13 +6,28 @@
 #     3、将数据保存到mongodb中
 
 import urllib2,json
+
 # 为笃定ip地址
 ip = '120.26.99.13'
 port = '4194'
+# 获取宿主机的信息url
+url_machine = 'http://' + ip + ':' + port + '/api/v1.0/machine'
 # 获取磁盘的url
 url_base_disk = 'http://' + ip + ':' + port + '/api/v1.0/containers'
 # 获取所有容器的url
 url_containers = url_base_disk + '/docker/'
+# 获取宿主机相关信息
+def get_machine(url):
+	'''
+		获取宿主机的所有信息磁盘、cpu、内存等等等
+	'''
+	info = json.loads(urllib2.urlopen(url).read())
+	cpu_total = info['cpu_frequency_khz']
+	memory_total  = info['topology'][0]['memory']
+	return {'cpu_total':cpu_total,'memory_total':memory_total}
+print get_machine(url_machine)
+
+
 # 获取磁盘使用信息
 def get_disk_info(url):
     '''
@@ -47,13 +62,18 @@ def get_containers(url):
 # 获取单个容器内所有信息
 def get_containers_info(url):
     info = json.loads(urllib2.urlopen(url).read())
+    '''
+    a = get_containers_info('http://120.26.99.13:4194/api/v1.0/containers/docker/ed3ff139938c0812d82dc44cdc4a5e1cefffbb45a38b925832d5b8fe41f0023a')
+	containers_id = a['aliases'][1]
+	image = a['spec']['image']
+	container_name = a['spec']['labels']['io.kubernetes.container.name']
+	svc_pod_name = a['spec']['labels']['io.kubernetes.pod.name']
+	restartCount = a['spec']['labels']['io.kubernetes.container.restartCount']
+	creation_time = a['spec']['creation_time']
+	cpu_usage = a['stats'][0]['cpu']['usage']['total']/2593816000000000.00 # 妈的 不知道cpu是以Hz为单位，暂定是百分比
+	memory_usage = a['stats'][0]['memory']['usage']/1024/1024.00 # 以M为单位
+	print a
+	print cpu_usage,memory_usage
+	'''
     return info
-a = get_containers_info('http://120.26.99.13:4194/api/v1.0/containers/docker/ed3ff139938c0812d82dc44cdc4a5e1cefffbb45a38b925832d5b8fe41f0023a')
-id = a['aliases'][1]
-image = a['spec']['image']
-container_name = a['spec']['labels']['io.kubernetes.container.name']
-svc_pod_name = a['spec']['labels']['io.kubernetes.pod.name']
-restartCount = a['spec']['labels']['io.kubernetes.container.restartCount']
-creation_time = a['spec']['creation_time']
-print a['spec']
 
