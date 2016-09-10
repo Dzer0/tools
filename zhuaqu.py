50795c5ae44cf78e74b81b5a9479947031a1faa4
Dzer0 <23442435@qq.com>
2016-09-10 10:42:10 +0800

    完成容器信息数据库

--- zhuaqu.py
+++ zhuaqu.py
@@ -2,20 +2,10 @@
 # author:Dzer0
 # Version:Python2.7.10
 # 思路:1、读取cAdvisor中的docker信息
-#     2、通过ansible来获取宿主机的cpu、内存信息
-#     3、将数据保存到mongodb中
+#     2、将数据保存到mongodb中
 
 import urllib2,json
 
-# 为笃定ip地址
-ip = '120.26.99.13'
-port = '4194'
-# 获取宿主机的信息url
-url_machine = 'http://' + ip + ':' + port + '/api/v1.0/machine'
-# 获取磁盘的url
-url_base_disk = 'http://' + ip + ':' + port + '/api/v1.0/containers'
-# 获取所有容器的url
-url_containers = url_base_disk + '/docker/'
 # 获取宿主机相关信息
 def get_machine(url):
 	'''
@@ -25,9 +15,6 @@ def get_machine(url):
 	cpu_total = info['cpu_frequency_khz']
 	memory_total  = info['topology'][0]['memory']
 	return {'cpu_total':cpu_total,'memory_total':memory_total}
-print get_machine(url_machine)
-
-
 # 获取磁盘使用信息
 def get_disk_info(url):
     '''
@@ -58,22 +45,45 @@ def get_containers(url):
     return list_containers
     #list_containers =  get_containers(url_containers)
 #print list_containers
-
 # 获取单个容器内所有信息
 def get_containers_info(url):
-    info = json.loads(urllib2.urlopen(url).read())
-    '''
-    a = get_containers_info('http://120.26.99.13:4194/api/v1.0/containers/docker/ed3ff139938c0812d82dc44cdc4a5e1cefffbb45a38b925832d5b8fe41f0023a')
-	containers_id = a['aliases'][1]
-	image = a['spec']['image']
-	container_name = a['spec']['labels']['io.kubernetes.container.name']
-	svc_pod_name = a['spec']['labels']['io.kubernetes.pod.name']
-	restartCount = a['spec']['labels']['io.kubernetes.container.restartCount']
-	creation_time = a['spec']['creation_time']
-	cpu_usage = a['stats'][0]['cpu']['usage']['total']/2593816000000000.00 # 妈的 不知道cpu是以Hz为单位，暂定是百分比
-	memory_usage = a['stats'][0]['memory']['usage']/1024/1024.00 # 以M为单位
-	print a
-	print cpu_usage,memory_usage
-	'''
-    return info
 
+    '''
+        # a = get_containers_info('http://120.26.99.13:4194/api/v1.0/containers/docker/ed3ff139938c0812d82dc44cdc4a5e1cefffbb45a38b925832d5b8fe41f0023a')
+    '''
+    dzero = []
+    info = json.loads(urllib2.urlopen(url).read())
+    containers_id = info['aliases'][1]
+    image = info['spec']['image']
+    try:
+        container_name = info['spec']['labels']['io.kubernetes.container.name']
+        svc_pod_name = info['spec']['labels']['io.kubernetes.pod.name']
+        restartCount = info['spec']['labels']['io.kubernetes.container.restartCount']
+    except Exception, e:
+        container_name = info['aliases'][0]
+        svc_pod_name = ''
+        restartCount = '0'
+    creation_time = info['spec']['creation_time']
+    cpu_usage = info['stats'][0]['cpu']['usage']['total']/(get_machine(url_machine)['cpu_total']*1000000.00) # 不知道为啥 要多加6个零才是正确的cpu占用比，坐等以后查询检查问题
+    memory_usage = info['stats'][0]['memory']['usage']/1024/1024.00 # 以M为单位
+    dzero.append({'containers_id':containers_id,'image':image,'container_name':container_name + '_' + svc_pod_name,'restartCount':restartCount,'creation_time':creation_time})
+    dzero.append({'containers_id':containers_id,'container_name':container_name + '_' + svc_pod_name,'cpu_usage':cpu_usage,'memory_usage':memory_usage})
+    return dzero
+
+if __name__ == '__main__':
+    # ip地址
+    ip = '120.26.99.13'
+    port = '4194'
+    # 获取宿主机的信息url，获取宿主的 cpu和内存
+    url_machine = 'http://' + ip + ':' + port + '/api/v1.0/machine'
+    # 获取磁盘的url
+    url_base_disk = 'http://' + ip + ':' + port + '/api/v1.0/containers'
+    # 获取所有容器的url
+    url_containers = url_base_disk + '/docker/'
+    containers_list = get_containers(url_containers)
+    for i in containers_list:
+        x = get_containers_info(i)
+        #print i
+        #n +=1
+        #print n
+        print x
